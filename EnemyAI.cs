09b65439365f57530b990bc29608c0414f64d30e
PeterQStarlord <kibblesamgreat@gmail.com>
2017-01-22 07:46:48 -0800

    Enemy Movement... ARRRRRRRRRRRRRRRRRRRRRR

---
+++ EnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform Target;
+	private GameObject Enemy;
+	private GameObject Player;
+	private float Range;
+	public float Speed;
+
+
+	// Use this for initialization
+	void Start () {
+		Enemy = GameObject.FindGameObjectWithTag ("Enemy");
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Range = Vector2.Distance (Enemy.transform.position, Player.transform.position);
+		if (Range >= 15f) {
+			transform.Translate(Vector2.MoveTowards (Enemy.transform.position, Player.transform.position, Range) * Speed * Time.deltaTime);
+		}
+	}
+}
