fbcce6e06752fd037834c95654a76d04e064a3b8
PeterQStarlord <kibblesamgreat@gmail.com>
2017-01-22 09:51:32 -0800

    Pause Men- ARRRRRRRRRRRRRRRRRRRRR

---
+++ PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public string resume;
+
+    public bool isPaused;
+
+    public GameObject pauseMenuCanvas;
+
+	void Update () {
+
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+            
+     }
+
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game has been Quit");
+        Application.Quit();
+    }
+}
